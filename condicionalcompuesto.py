2a7b5f2c9431337dd1ea851f26d842a4f238846b
Lmhc199301* <luis.tecnicotele@gmail.com>
2025-07-24 20:33:25 -0600

    Nuevo ejemplo

---
+++ condicionalcompuesto.py
@@ -0,0 +1,12 @@
+"""
+ingrese numero si cero es neutro, y determine si es positivo o negativo
+"""
+numero=int(input("Ingrese numero :"))
+if numero==0:
+    print(f" El {numero} es nuetro ")
+elif numero>0:
+    print(f" El {numero} es positivo ")
+else:
+    print(f" El {numero} es negativo ")
+
+
